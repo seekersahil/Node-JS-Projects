160be2702fb35e2cc322dd374a9bbc7a19ec033e
Sahil Gupta <seekersahil@gmail.com>
2023-01-17 11:05:44 +0530

    updates

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -1,12 +1,13 @@
-const { readFileSync, writeFileSync } = require('fs')
-console.log('start')
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const { readFileSync, writeFileSync } = require("fs");
+console.log("start");
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
 
 writeFileSync(
-  './content/result-sync.txt',
+  "./content/result-sync.txt",
   `Here is the result : ${first}, ${second}`,
-  { flag: 'a' }
-)
-console.log('done with this task')
-console.log('starting the next one')
+  { flag: "a" }
+);
+// flag: "a" appends rather than replacing existing
+console.log("done with this task");
+console.log("starting the next one");
